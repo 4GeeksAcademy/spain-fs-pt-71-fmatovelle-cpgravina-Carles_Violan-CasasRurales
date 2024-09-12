8aac79f8a966a0b82342586b1c32b2104b659831
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-09-12 00:35:17 +0000

    done

--- src/front/js/component/houseFeatures/index.js
+++ src/front/js/component/houseFeatures/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import './styles.css'; // Import the styles
+import './styles.css'; 
 
 export const HouseFeatures = ({ features }) => {
   return (
     <div className="house-features-container">
       <h3 className="features-title">Features</h3>
-      <ul className="features-list"> {/* Use the updated class names for styles */}
+      <ul className="features-list"> 
         <li>Square Meters: {features?.square_meters}</li>
         <li>Bedrooms: {features?.bedrooms}</li>
         <li>Bathrooms: {features?.bathrooms}</li>
